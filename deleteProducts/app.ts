9e8923b4c622ef86288830f5459266203df33af0
bobandash <brucehsu1126@gmail.com>
2024-09-12 15:33:36 -0700

    Add: connection pools instead of connections

--- deleteProducts/app.ts
+++ deleteProducts/app.ts
@@ -1,95 +1,68 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
-import aws from 'aws-sdk';
-import { Client } from 'pg';
 import { composeGid } from '@shopify/admin-graphql-api-utilities';
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+import { APIGatewayProxyResult } from 'aws-lambda';
+import { SecretsManager } from 'aws-sdk';
+import { Pool, PoolClient } from 'pg';
 
 type ProductDeletePayload = {
-    id: string;
+    id: number;
 };
 
-const secretManagerClient = new aws.SecretsManager();
+let pool: Pool | null = null;
+const secretManagerClient = new SecretsManager();
+
+async function getDbConfig() {
+    const secretManagerResult = await secretManagerClient
+        .getSecretValue({
+            SecretId: process.env.SECRET_MANAGER_ID ?? '',
+        })
+        .promise();
+    return JSON.parse(secretManagerResult.SecretString as string);
+}
+
+async function initializePool() {
+    if (!pool) {
+        const dbConfig = await getDbConfig();
+        pool = new Pool(dbConfig);
+    }
+    return pool;
+}
 
 export const lambdaHandler = async (event: ProductDeletePayload): Promise<APIGatewayProxyResult> => {
-    let client: Client | null = null;
+    let client: null | PoolClient = null;
     try {
-        const secretManagerResult = await secretManagerClient
-            .getSecretValue({
-                SecretId: process.env.SECRET_MANAGER_ID ?? '',
-            })
-            .promise();
-        const cred = JSON.parse(secretManagerResult.SecretString as string);
-        const { user, password, host, port, database } = cred;
-        client = new Client({
-            user,
-            password,
-            host,
-            port,
-            database,
-        });
-        await client.connect();
+        const pool = await initializePool();
         const { id } = event;
         const shopifyDeletedProductId = composeGid('Product', id);
+        client = await pool.connect();
         const deleteProductQuery = 'DELETE FROM "Product" WHERE shopifyProductId = $1';
-        await client.query(deleteProductQuery, [shopifyDeletedProductId]);
+        const res = await client.query(deleteProductQuery, [shopifyDeletedProductId]);
+
+        if (res.rowCount) {
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    message: 'There were no products to delete.',
+                }),
+            };
+        }
+
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'Successfully deleted products from database',
+                message: 'Successfully deleted products from database.',
             }),
         };
-    } catch {
+    } catch (error) {
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'Could not delete products',
+                message: 'Could not delete products.',
+                error: (error as Error).message,
             }),
         };
     } finally {
         if (client) {
-            await client.end();
+            client.release();
         }
     }
 };
-
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
-
-// import {
-//     SecretsManagerClient,
-//     GetSecretValueCommand,
-//   } from "@aws-sdk/client-secrets-manager";
-
-//   const secret_name = "rds-db-credentials/synqsell-db-dev/postgres/1726084554591";
-
-//   const client = new SecretsManagerClient({
-//     region: "us-east-2",
-//   });
-
-//   let response;
-
-//   try {
-//     response = await client.send(
-//       new GetSecretValueCommand({
-//         SecretId: secret_name,
-//         VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
-//       })
-//     );
-//   } catch (error) {
-//     // For a list of exceptions thrown, see
-//     // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-//     throw error;
-//   }
-
-//   const secret = response.SecretString;
-
-//   // Your code goes here
