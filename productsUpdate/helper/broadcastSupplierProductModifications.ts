378c3d81b4905fec30edb0fc39d53e888cf052aa
bobandash <brucehsu1126@gmail.com>
2024-09-19 14:59:21 -0700

    Add: logic to update prices in database

--- productsUpdate/helper/broadcastSupplierProductModifications.ts
+++ productsUpdate/helper/broadcastSupplierProductModifications.ts
@@ -1,4 +1,4 @@
-import { PoolClient } from 'pg';
+import { Pool, PoolClient } from 'pg';
 import { EditedVariant } from '../types';
 import { createMapToRestObj, mutateAndValidateGraphQLData } from '../util';
 import { PRODUCT_VARIANT_BULK_UPDATE } from '../graphql';
@@ -25,6 +25,174 @@ type GroupedQueryDataWithUpdateFields = Map<
         }[];
     }
 >;
+
+type PriceListDetails = {
+    id: string;
+    createdAt: Date;
+    pricingStrategy: string;
+    supplierId: string;
+    isGeneral: boolean;
+    name: string;
+    requiresApprovalToImport?: boolean;
+    margin?: number;
+};
+
+export const PRICE_LIST_PRICING_STRATEGY = {
+    WHOLESALE: 'WHOLESALE',
+    MARGIN: 'MARGIN',
+} as const;
+
+type ShopifyVariantIdAndSupplierProfit = {
+    shopifyVariantId: string;
+    supplierProfit: string;
+};
+
+// Price Lists can have two strategies: a pricing strategy based on margin and pricing strategy based on fixed wholesale rate
+// If it's based on margin, we have to recalculate the supplier profit
+// If it's based on fixed wholesale rate, we don't have to calculate supplier profit
+// START: Functions related to updating prices in the database
+function round(value: number, decimals: number): number {
+    const factor = Math.pow(10, decimals);
+    const roundedValue = Math.round(value * factor) / factor;
+    return roundedValue;
+}
+
+function getNewPricingDetails(editedVariants: EditedVariant[], priceList: PriceListDetails) {
+    if (priceList.pricingStrategy !== PRICE_LIST_PRICING_STRATEGY.MARGIN) {
+        throw new Error('Cannot include supplier profit calculation for margin price list.');
+    }
+
+    const margin = priceList.margin;
+    if (!margin) {
+        throw new Error('Margin rate is undefined in price list, even though price list is margin.');
+    }
+    const marginPercentage = margin / 100;
+
+    return editedVariants.map((variant) => {
+        const retailPrice = Number(variant.price);
+        const retailerPayment = round(retailPrice * marginPercentage, 2);
+        const supplierProfit = round(retailPrice - retailerPayment, 2);
+
+        // Convert to string w/ two decimals to match database fields
+        const retailPriceStr = retailPrice.toFixed(2);
+        const retailerPaymentStr = retailerPayment.toFixed(2);
+        const supplierProfitStr = supplierProfit.toFixed(2);
+
+        return {
+            shopifyVariantId: variant.shopifyVariantId,
+            retailPrice: retailPriceStr,
+            retailerPayment: retailerPaymentStr,
+            supplierProfit: supplierProfitStr,
+        };
+    });
+}
+
+async function handleUpdateVariantsInMarginPriceList(
+    editedVariants: EditedVariant[],
+    priceList: PriceListDetails,
+    dbProductId: string,
+    client: PoolClient,
+) {
+    const variantPrices = getNewPricingDetails(editedVariants, priceList);
+    const updateVariantPriceQuery = `
+        UPDATE "Variant"
+        SET "retailPrice" = $1,
+            "retailerPayment" = $2,
+            "supplierProfit" = $3
+        WHERE
+            "shopifyVariantId" = $4 AND
+            "productId" = $5
+    `;
+    await Promise.all(
+        variantPrices.map(({ shopifyVariantId, retailPrice, retailerPayment, supplierProfit }) => {
+            return client.query(updateVariantPriceQuery, [
+                retailPrice,
+                retailerPayment,
+                supplierProfit,
+                shopifyVariantId,
+                dbProductId,
+            ]);
+        }),
+    );
+}
+
+async function handleUpdateVariantsInWholesalePriceList(
+    editedVariants: EditedVariant[],
+    dbProductId: string,
+    client: PoolClient,
+) {
+    const shopifyVariantIdAndSupplierProfitQuery = `
+        SELECT 
+            "Variant"."shopifyVariantId" AS "shopifyVariantId", 
+            "Variant"."supplierProfit" AS "supplierProfit"
+        FROM "Variant"
+        WHERE "Variant"."productId" = $1 
+    `;
+    const shopifyVariantIdAndSupplierProfitRes: ShopifyVariantIdAndSupplierProfit[] = (
+        await client.query(shopifyVariantIdAndSupplierProfitQuery)
+    ).rows;
+
+    const shopifyVariantIdToSupplierProfit = createMapToRestObj(
+        shopifyVariantIdAndSupplierProfitRes,
+        'shopifyVariantId',
+    );
+    const updateVariantPriceQuery = `
+        UPDATE "Variant"
+        SET "retailPrice" = $1,
+            "retailerPayment" = $2,
+        WHERE
+            "shopifyVariantId" = $3 AND
+            "productId" = $4
+    `;
+
+    await Promise.all(
+        editedVariants.map(({ shopifyVariantId, price }) => {
+            const supplierProfit = shopifyVariantIdToSupplierProfit.get(shopifyVariantId);
+            const retailerPayment = round(Number(price) - Number(supplierProfit), 2).toFixed(2);
+            return client.query(updateVariantPriceQuery, [price, retailerPayment, shopifyVariantId, dbProductId]);
+        }),
+    );
+}
+
+async function updateVariantPricesDatabase(
+    editedVariants: EditedVariant[],
+    supplierShopifyProductId: string,
+    client: PoolClient,
+) {
+    const priceListDetailsQuery = `
+            SELECT "PriceList".* FROM "Product"
+            INNER JOIN "PriceList" ON "Product"."priceListId" = "PriceList"."id"
+            WHERE "Product"."shopifyProductId" = $1
+        `;
+    const res = await client.query(priceListDetailsQuery, [supplierShopifyProductId]);
+    if (res.rows.length === 0) {
+        throw new Error('No price list found.');
+    }
+
+    // the same product can be in multiple price lists
+    const priceLists: PriceListDetails[] = res.rows;
+    priceLists.forEach(async (priceList) => {
+        const dbProductIdQuery = `
+            SELECT "Product"."id" AS "dbProductId" FROM "Variant"
+            INNER JOIN "Product" ON "Product"."id" = "Variant"."productId"
+            WHERE 
+                "Product"."priceListId" = $1 AND
+                "Product"."shopifyProductId" = $2
+            LIMIT 1
+        `;
+        const dbProductIdRes = await client.query(dbProductIdQuery);
+        if (dbProductIdRes.rows.length === 0) {
+            throw new Error('Product for specified price list and shopify product id does not exist in database.');
+        }
+        const dbProductId: string = dbProductIdRes.rows[0].dbProductId;
+        if (priceList.pricingStrategy === PRICE_LIST_PRICING_STRATEGY.MARGIN) {
+            handleUpdateVariantsInMarginPriceList(editedVariants, priceList, dbProductId, client);
+        } else if (priceList.pricingStrategy === PRICE_LIST_PRICING_STRATEGY.WHOLESALE) {
+            handleUpdateVariantsInWholesalePriceList(editedVariants, dbProductId, client);
+        }
+    });
+}
+// EMD: Functions related to updating prices in the database
 
 async function updateRetailerDataOnShopify(data: GroupedQueryDataWithUpdateFields) {
     try {
@@ -73,7 +241,7 @@ function getRetailerProductData(
         const supplierVariantDetails = editedVariantMap.get(row.supplierShopifyVariantId);
         const newRetailPrice = supplierVariantDetails?.price;
         const newInventory = supplierVariantDetails?.newInventory;
-        if (!newRetailPrice || !newInventory) {
+        if (newRetailPrice === undefined || newInventory === undefined) {
             throw new Error('Retail price or inventory is not defined.');
         }
 
@@ -98,7 +266,6 @@ function getRetailerProductData(
 
 // if the supplier changes the inventory or retail price, it should be updated for all the retailers that imported the product
 // key is supplier shopify variant id, values inside map are the remaining fields
-// TODO: handle update data in prisma
 async function broadcastSupplierProductModifications(
     editedVariants: EditedVariant[],
     supplierShopifyProductId: string,
@@ -127,7 +294,10 @@ async function broadcastSupplierProductModifications(
     }
     const data: QueryData[] = res.rows;
     const retailerProductData = getRetailerProductData(data, editedVariantMap);
-    await updateRetailerDataOnShopify(retailerProductData);
+    await Promise.all([
+        updateRetailerDataOnShopify(retailerProductData),
+        updateVariantPricesDatabase(editedVariants, supplierShopifyProductId, client),
+    ]);
 }
 
 export default broadcastSupplierProductModifications;
