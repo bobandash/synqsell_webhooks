4307e566340505a450bdf7a32c82ba3a586d5e8a
bobandash <brucehsu1126@gmail.com>
2024-09-19 03:23:04 -0700

    Add: heplful reusable functions to template

--- .template.lambda/app.ts
+++ .template.lambda/app.ts
@@ -1,29 +1,7 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { PoolClient } from 'pg';
 import { initializePool } from './db';
-
-// Command to debug deleteProducts locally
-// sam local invoke DeleteProductsLambda --event ./deleteProducts/app_event.json
-
-type ShopifyEvent = {
-    version: string;
-    id: string;
-    'detail-type': string;
-    source: string;
-    account: string;
-    time: string;
-    region: string;
-    resources: string[];
-    detail: {
-        'X-Shopify-Topic': string;
-        'X-Shopify-Hmac-Sha256': string;
-        'X-Shopify-Shop-Domain': string;
-        'X-Shopify-Webhook-Id': string;
-        'X-Shopify-Triggered-At': string;
-        'X-Shopify-Event-Id': string;
-        payload: { sample: 'sample' }; // TODO: add payload details
-    };
-};
+import { ShopifyEvent } from './types';
 
 export const lambdaHandler = async (event: ShopifyEvent): Promise<APIGatewayProxyResult> => {
     let client: null | PoolClient = null;
---
+++ .template.lambda/types.ts
@@ -0,0 +1,64 @@
+export type ShopifyEvent = {
+    version: string;
+    id: string;
+    'detail-type': string;
+    source: string;
+    account: string;
+    time: string;
+    region: string;
+    resources: string[];
+    detail: {
+        'X-Shopify-Topic': string;
+        'X-Shopify-Hmac-Sha256': string;
+        'X-Shopify-Shop-Domain': string;
+        'X-Shopify-Webhook-Id': string;
+        'X-Shopify-Triggered-At': string;
+        'X-Shopify-Event-Id': string;
+        payload: {
+            admin_graphql_api_id: string;
+            body_html: string | null;
+            created_at: string | null;
+            handle: string;
+            id: number;
+            product_type: string;
+            published_at: string;
+            template_suffix: string | null;
+            title: string;
+            updated_at: string;
+            vendor: string;
+            status: string;
+            published_scope: string;
+            tags: string;
+            variants: {
+                admin_graphql_api_id: string;
+                barcode: string | null;
+                compare_at_price: string;
+                created_at: string;
+                id: number;
+                inventory_policy: string;
+                position: number;
+                price: string;
+                product_id: number;
+                sku: string | null;
+                taxable: boolean;
+                title: string;
+                updated_at: string;
+                option1: string;
+                option2: string | null;
+                option3: string | null;
+                image_id: number | null;
+                inventory_item_id: number | null;
+                inventory_quantity: number;
+                old_inventory_quantity: number;
+            }[];
+            options: any[];
+            images: any[];
+            image: any | null;
+            media: any[];
+            variant_gids: {
+                admin_graphql_api_id: string;
+                updated_at: string;
+            }[];
+        };
+    };
+};
