9e920a237ea2c9d5c234acd4c7e114f9cde1a4f4
bobandash <brucehsu1126@gmail.com>
2024-09-24 21:57:58 -0700

    Add: template for carrier service

---
+++ carrierService/types.ts
@@ -0,0 +1,40 @@
+type Address = {
+    country: string;
+    postal_code: string;
+    province: string;
+    city: string;
+    name: string | null;
+    address1: string;
+    address2: string;
+    address3: string | null;
+    phone: string | null;
+    fax: string | null;
+    email: string | null;
+    address_type: string | null;
+    company_name: string | null;
+};
+
+type Item = {
+    name: string;
+    sku: string;
+    quantity: number;
+    grams: number;
+    price: number;
+    vendor: string;
+    requires_shipping: boolean;
+    taxable: boolean;
+    fulfillment_service: string;
+    properties: null;
+    product_id: number;
+    variant_id: number;
+};
+
+type RequestBody = {
+    rate: {
+        origin: Address;
+        destination: Address;
+        items: Item[];
+        currency: string;
+        locale: string;
+    };
+};
